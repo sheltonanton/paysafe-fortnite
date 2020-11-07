c477d9429443fb9050ee09357ba29b7ab3aad81f
Your <name@mail.com>
2020-11-07 15:30:23 +0530

    btoa package introduced

--- scripts/paysafePaymentApi.js
+++ scripts/paysafePaymentApi.js
@@ -1,3 +1,4 @@
+const btoa = require('btoa');
 const PAYSAFE_API = "https://api.test.paysafe.com/paymenthub/v1";
 const PAYSAFE_AUTH = `Basic ${btoa('private-7751:B-qa2-0-5f031cdd-0-302d0214496be84732a01f690268d3b8eb72e5b8ccf94e2202150085913117f2e1a8531505ee8ccfc8e98df3cf1748')}`;
 
